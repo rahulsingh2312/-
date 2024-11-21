6d67db8de0124dfda9157080d93aedad4ef569e4
rahulsingh2312 <rahulsinghhh2312@gmail.com>
2024-11-21 13:27:38 +0530

    graph

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import { useState } from "react";
 
 import SwapYen from "./components/swapYen";
 import Footer from "./components/Footer";
@@ -10,15 +9,50 @@ import Navbar from "./components/navbar";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
 
   return (
     <div className="h-screen">
-     
       <RainbowMountain />
       <Matrix />
       <SwapYen />
       <WallOfMeme />
+     {/* Community Cult Badge */}
+<div className="px-6 my-10 py-1 text-xl barrio-regular rounded-full border border-gray-500 mx-auto text-center z-10 bg-white/20 backdrop-blur-sm w-fit flex items-center gap-2">
+  <span className="line-through">Community</span> Cult of
+  <img
+    className="md:w-5 w-4"
+    src="/yenemoji.webp"
+    alt="Yen emoji"
+  />
+</div>
+
+      {/* Embed Dexscreener in a proper way */}
+      <div
+
+      className="flex justify-center items-center"
+        id="dexscreener-embed"
+        style={{
+          position: "relative",
+          width: "90%",
+          margin: "auto",
+         
+          // paddingBottom: "125%",
+        }}
+      >
+        <iframe
+          src="https://dexscreener.com/solana/6jua8dYfLMhyfjnGYXd2YjJipQJLnriboXneCti7CHDX?embed=1&loadChartSettings=0&tabs=0&info=0&chartLeftToolbar=0&chartDefaultOnMobile=1&chartTheme=dark&theme=dark&chartStyle=0&chartType=usd&interval=15"
+          style={{
+            // position: "absolute",
+            width: "100vw",
+            height: "100vh",
+           justifyContent: "center",
+           alignItems: "center",
+           display: "flex",
+          }}
+          title="Dexscreener Embed"
+        />
+      </div>
+
       <Footer />
       <ActualFooter />
     </div>
